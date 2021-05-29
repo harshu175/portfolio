b9560e3ff20ea0d3002128185e375ac654de5d8d
Harshit Pandey <harshitp175@gmail.com>
2021-05-29 20:05:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.scroll-up-btn').addClass("show");
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY > 300){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+        
+        
+    })
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop : 0})
+    })
+
+    // toggle menubar
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    })
+
+    $("#hire").click(function(e){
+        alert("Send Mail To Email mentioned in contact section");
+    })
+
+    //typing animation
+
+    var typed = new Typed(".typing" , {
+        strings : ["Software Developer" ,  "Engineer", "Student", "Coder"],
+        typeSpeed : 100,
+        backSpeed: 60,
+        loop : true
+    })
+
+    var typed = new Typed(".typing-2" , {
+        strings : ["Software Developer" ,  "Engineer", "Student", "Coder"],
+        typeSpeed : 100,
+        backSpeed: 60,
+        loop : true
+    })
+
+    //owl-carousel script
+    $('.carousel').owlCarousel({
+        margin : 20,
+        loop : false,
+        autoplayTimeOut : 2000,
+        autoplayHoverPause : true,
+        reponsive : {
+            0:{
+                items : 1,
+                nav : false,
+            },
+            600:{
+                items:2,
+                nav : false,
+            },
+            1000:{
+                items:3,
+                nav : false,
+            }
+        }
+    })
+
+    $('#img').mouseenter(function (e) {
+        if(e.button == 2) { // right click
+          return false; // do nothing!
+        }
+    })
+});
