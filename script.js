b6496d40105c3f19b17dbdbd88b454eb99339f25
Harshit Pandey <harshitp175@gmail.com>
2021-05-30 14:56:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,14 +32,14 @@ $(document).ready(function(){
     //typing animation
 
     var typed = new Typed(".typing" , {
-        strings : ["Software Developer" ,  "Engineer", "Student", "Coder"],
+        strings : ["Software Developer" ,  "Engineer", "Student", "PepCoder"],
         typeSpeed : 100,
         backSpeed: 60,
         loop : true
     })
 
     var typed = new Typed(".typing-2" , {
-        strings : ["Software Developer" ,  "Engineer", "Student", "Coder"],
+        strings : ["Software Developer" ,  "Engineer", "Student", "PepCoder"],
         typeSpeed : 100,
         backSpeed: 60,
         loop : true
